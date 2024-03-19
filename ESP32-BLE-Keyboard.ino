fb94e1635e2dd1fdd37620db25f6db639121e6e7
Jesse Cornman <ocanath@gmail.com>
2024-03-19 05:01:31 -0700

    example moved

--- ESP32-BLE-Keyboard.ino
+++ ESP32-BLE-Keyboard.ino
@@ -1,12 +1,12 @@
 /**
  * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
  */
-#include <BleKeyboard.h>
+#include "BleKeyboard.h"
 
 BleKeyboard bleKeyboard;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(460800);
   Serial.println("Starting BLE work!");
   bleKeyboard.begin();
 }
